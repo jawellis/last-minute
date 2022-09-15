96adbbcf176b9bca9ae2d368492acff7324bc23f
Ellis <ellissiadis@gmail.com>
2022-09-15 15:44:46 +0200

    docs: add migration file for notices to make new db table

---
+++ database/migrations/2022_09_15_131416_create_notices_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNoticesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notices', function (Blueprint $table) {
+            $table->id();
+            $table->string('name'); // name
+            $table->time('from_time'); // from what time
+            $table->time('until_time'); // until what time
+            $table->string('location'); // Location
+            $table->string('day_part_tags');
+            $table->boolean('active'); //Active or non-active notice
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notices');
+    }
+}
